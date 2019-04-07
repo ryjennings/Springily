630ff7b8bf6129002aac4a4992a9eee1ecdfa72e
Ryan Jennings <ryjennings@gmail.com>
2019-04-07 13:29:30 -0700

    Initial Commit

---
+++ Springily/Springily.h
@@ -0,0 +1,19 @@
+//
+//  Springily.h
+//  Springily
+//
+//  Created by Ryan Jennings on 4/7/19.
+//  Copyright © 2019 Ryan Jennings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Springily.
+FOUNDATION_EXPORT double SpringilyVersionNumber;
+
+//! Project version string for Springily.
+FOUNDATION_EXPORT const unsigned char SpringilyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Springily/PublicHeader.h>
+
+
